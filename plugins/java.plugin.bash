b7f33c99b30c2aa3ec1dfc5e02bd9ae9a2bd3995
Noorul Islam K M <noorul@noorul.com>
2016-11-01 11:41:57 +0530

    Use java version 1.8

--- plugins/java.plugin.bash
+++ plugins/java.plugin.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 # export JAVA_HOME=/opt/jdk1.7.0_25
 # export PATH=$PATH:/opt/jdk1.7.0_25/bin
